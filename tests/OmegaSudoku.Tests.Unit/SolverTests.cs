ec61d92556d045b06e7db2ace8b5729bc8bdf100
Shirranyuval07 <shirranyuval07@gmail.com>
2026-01-19 08:41:14 +0200

    added test for sudoku file with sudokus cantaining 17 clues

--- tests/OmegaSudoku.Tests.Unit/SolverTests.cs
+++ tests/OmegaSudoku.Tests.Unit/SolverTests.cs
@@ -11,7 +11,7 @@ namespace OmegaSudoku.Tests.Unit
         [Fact]
 
         //testing 1 million sudoku board (takes about 12 minutes)
-        public void SolveSudoku_TestSolution()
+        public void SolveSudoku_TestSolutions()
         {
             List<string> listA = new List<string>();
             List<string> listB = new List<string>();
@@ -41,6 +41,40 @@ namespace OmegaSudoku.Tests.Unit
                 if (board.ToString() != expectedSolution)
                 {
                     throw new Exception("TestSolveSudoku failed: The solved board does not match the expected solution.");
+                }
+            }
+        }
+
+        //testing 17 clue sudoku boards 
+        [Fact]
+        public void SolveSudokuTest17Clue()
+        {
+            List<string> listA = new List<string>();
+
+            using (var reader = new StreamReader(@"C:\Users\Owner\Yuval_Omega\OmegaSudoku\OmegaSudoku\bin\Debug\17_clue.txt"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+
+                    listA.Add(line);
+                }
+            }
+
+            foreach (string puzzle in listA)
+            {
+                
+                try
+                {
+                    // Arrange
+                    SudokuBoard board = new SudokuBoard(puzzle);
+                    // Act
+                    Solver.Solve(board);
+                }
+                catch (Exception ex)
+                {
+                    // Assert
+                    throw new Exception("TestSolveSudoku failed: An exception occurred while solving the puzzle.", ex);
                 }
             }
         }
